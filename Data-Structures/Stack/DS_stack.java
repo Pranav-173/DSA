2671368c887c1e7ba9ecb3b0a6f3dbe578fbcc14
Amal Bijoy <amalbijoy2007@gmail.com>
2026-04-01 14:18:36 +0530

    Refactor stack messages and formatting

--- Data-Structures/Stack/DS_stack.java
+++ Data-Structures/Stack/DS_stack.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+
 class Stack {
 	static int MAX_SIZE;
 	static int[] stack;
 	static int top;
-	public Stack(int x){
+	public Stack(int x) {
 		MAX_SIZE = x;
 		stack = new int[MAX_SIZE];
 		top = -1;
@@ -16,44 +17,44 @@ class Stack {
 	}
 	static void push(int value) {
 		if (isFull()) {
-			System.out.println("Stack Overflow! Cannot push " + value);
+			System.out.println("Stack Overflow! Cannot Push " + value);
 			return;
 		}
 		stack[++top] = value;
-		System.out.println(value + " pushed to stack");
+		System.out.println(value + " Pushed to Stack.");
 	}
 	static int pop() {
 		if (isEmpty()) {
-			System.out.println("Stack Underflow! Cannot pop");
+			System.out.println("Stack Underflow! Cannot Pop.");
 			return -1;
 		}
-		System.out.println(stack[top] + " popped from stack");
+		System.out.println(stack[top] + " Popped from Stack.");
 		return stack[top--];
 	}
 	static void display() {
 		if (isEmpty()) {
-			System.out.println("Stack is empty!");
+			System.out.println("Stack is Empty!");
 			return;
 		}
 		System.out.print("Stack: ");
 		for (int i = 0; i <= top; i++) {
-			System.out.print(stack[i] + " ");
+			System.out.println(stack[i] + " ");
 		}
 		System.out.println();
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the stack size: ");
+		System.out.println("Enter the Stack size: ");
 		int size = sc.nextInt();
 		Stack stack = new Stack(size);
-		while(true){
+		while (true) {
 			System.out.println("***** Stack Operations *****");
 			System.out.println("1. Push\n2. Pop\n3. Display\n4. Exit");
 			System.out.println("Enter your choice: ");
 			int choice = sc.nextInt();
 			switch (choice) {
 				case 1:
-					System.out.print("Enter value to push: ");
+					System.out.print("Enter value to Push: ");
 					int value = sc.nextInt();
 					stack.push(value);
 					break;
