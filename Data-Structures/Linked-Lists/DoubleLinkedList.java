156e46c425efec256e3d764edb9c623f389b7693
Amal Bijoy <amalbijoy2007@gmail.com>
2026-04-04 19:19:29 +0530

    Update DoubleLinkedList.java

--- Data-Structures/Linked-Lists/DoubleLinkedList.java
+++ Data-Structures/Linked-Lists/DoubleLinkedList.java
@@ -4,16 +4,20 @@ class Node {
     int data;
     Node prev;
     Node next;
+
     Node(int data) {
         this.data = data;
         this.prev = null;
         this.next = null;
     }
 }
-class doublylinkedlist {
+
+class DoublyLinkedList {
     private Node head;
+
     public void insertAtBeginning(int data) {
         Node newNode = new Node(data);
+
         if (head == null) {
             head = newNode;
         } else {
@@ -21,58 +25,74 @@ class doublylinkedlist {
             head.prev = newNode;
             head = newNode;
         }
-        System.out.println(data + " Inserted from Beginning.");
+
+        System.out.println(data + " Inserted at the Beginning.");
     }
+
     public void insertAtEnd(int data) {
         Node newNode = new Node(data);
+
         if (head == null) {
             head = newNode;
             System.out.println(data + " Inserted at the End.");
             return;
         }
+
         Node temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
+
         temp.next = newNode;
         newNode.prev = temp;
+
         System.out.println(data + " Inserted at the End.");
     }
+
     public void deleteFromBeginning() {
         if (head == null) {
             System.out.println("The List is Empty. Can't Delete element.");
             return;
         }
-        System.out.println(head.data + " Deleted from Front.");
+
+        System.out.println(head.data + " Deleted from Beginning.");
         head = head.next;
+
         if (head != null) {
             head.prev = null;
         }
     }
+
     public void deleteFromEnd() {
         if (head == null) {
             System.out.println("The List is Empty. Can't Delete element.");
             return;
         }
+
         if (head.next == null) {
             System.out.println(head.data + " Deleted from End.");
             head = null;
             return;
         }
+
         Node temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
+
         System.out.println(temp.data + " Deleted from End.");
         temp.prev.next = null;
     }
+
     public void search(int key) {
         if (head == null) {
             System.out.println("The List is Empty!");
             return;
         }
+
         Node temp = head;
         int pos = 1;
+
         while (temp != null) {
             if (temp.data == key) {
                 System.out.println(key + " Found at position: " + pos);
@@ -81,31 +101,38 @@ class doublylinkedlist {
             temp = temp.next;
             pos++;
         }
+
         System.out.println(key + " NOT Found in the List.");
     }
-    public void displayforward() {
+
+    public void displayForward() {
         if (head == null) {
             System.out.println("The List is Empty!");
             return;
         }
+
+        System.out.println("Forward Traversal:");
         Node temp = head;
-        System.out.println("Forward Traversal: ");
+
         while (temp != null) {
             System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
         System.out.println("null");
     }
-    public void displaybackward() {
+
+    public void displayBackward() {
         if (head == null) {
             System.out.println("The List is Empty!");
             return;
         }
+
         Node temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
-        System.out.println("Backward Traversal: ");
+
+        System.out.println("Backward Traversal:");
         while (temp != null) {
             System.out.print(temp.data + " -> ");
             temp = temp.prev;
@@ -113,13 +140,15 @@ class doublylinkedlist {
         System.out.println("null");
     }
 }
+
 public class DoubleLinkedList {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        doublylinkedlist dll = new doublylinkedlist();
-        int choice, data, position;
+        DoublyLinkedList dll = new DoublyLinkedList();
+        int choice, data;
+
         do {
-            System.out.println("\n---Linked List Operations Menu---");
+            System.out.println("\n--- Linked List Operations Menu ---");
             System.out.println("1. Insert at Beginning");
             System.out.println("2. Insert at End");
             System.out.println("3. Delete from Beginning");
@@ -129,42 +158,53 @@ public class DoubleLinkedList {
             System.out.println("7. Display Backward Direction");
             System.out.println("8. Exit");
             System.out.print("Enter your choice: ");
+
             choice = sc.nextInt();
+
             switch (choice) {
                 case 1:
                     System.out.print("Enter element to Insert at Beginning: ");
                     data = sc.nextInt();
                     dll.insertAtBeginning(data);
                     break;
+
                 case 2:
                     System.out.print("Enter element to Insert at End: ");
                     data = sc.nextInt();
                     dll.insertAtEnd(data);
                     break;
+
                 case 3:
                     dll.deleteFromBeginning();
                     break;
+
                 case 4:
                     dll.deleteFromEnd();
                     break;
+
                 case 5:
                     System.out.print("Enter element to Search: ");
                     data = sc.nextInt();
                     dll.search(data);
                     break;
+
                 case 6:
-                    dll.displayforward();
+                    dll.displayForward();
                     break;
+
                 case 7:
-                    dll.displaybackward();
+                    dll.displayBackward();
                     break;
+
                 case 8:
                     System.out.println("Exiting...");
                     break;
+
                 default:
                     System.out.println("Invalid choice!");
             }
         } while (choice != 8);
+
         sc.close();
     }
 }
